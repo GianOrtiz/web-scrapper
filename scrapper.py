5cd4e340e7ec54be5873bd5d68f1e64ef28bfe81
Gian Ortiz <brksgian@gmail.com>
2024-10-12 08:36:30 -0300

    Add headers

--- scrapper.py
+++ scrapper.py
@@ -17,15 +17,22 @@ def get_content(cache=False):
             content = f.read()
             return content
     else:
-        headers = { "User-Agent": "Mozilla/5.0" }
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36",
+            "Accept-Encoding": "gzip, deflate, br", 
+	        "Accept-Language": "en-US,en;q=0.9", 
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+
+        }
         page_request = requests.get(links[0], headers=headers)
         content = page_request.content
+        print(content)
         # Writes the received content to a cache to avoid unecessary requests.
-        with open(cach_file, 'w+') as f:
-            f.write(conteudo.decode('utf-8'))
+        with open(cache_file, 'w+') as f:
+            f.write(content.decode('utf-8'))
         return content
         
-content = get_content(True)
+content = get_content(False)
 site = BeautifulSoup(content, 'html.parser')
  
 products = site.find_all(attrs={"data-testid": "product-card-container"})
