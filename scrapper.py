fa6da8f2f9be8c2caa1fa8cdbb7e28e75277e82a
Gian Ortiz <brksgian@gmail.com>
2024-10-15 21:25:23 -0300

    Retrieve all pages from a website

--- scrapper.py
+++ scrapper.py
@@ -1,25 +1,51 @@
+import time
+
 from bs4 import BeautifulSoup
 from cache import Cache
 from strategy.factory import ScrappingStrategyFactory
+from data.product import Product
+from urllib.parse import urlparse
 
 links = [
     "https://www.magazineluiza.com.br/lava-e-seca/eletrodomesticos/s/ed/ela1/?page=1",
-    "https://www.magazineluiza.com.br/lava-e-seca/eletrodomesticos/s/ed/ela1/?page=2",
 ]
 
 class Scrapper:
-    def __init__(self, cache: Cache):
+    def __init__(self, cache: Cache, links: list[str]):
+        self.__links = links
         self.__cache: Cache = cache
         self.__scrapping_strategy_factory: ScrappingStrategyFactory = ScrappingStrategyFactory()
 
-    def scrap(self, page: str):
+    def scrap_all_sites(self) -> list[Product]:
+        products: list[Product] = []
+        for link in self.__links:
+            print('Retrieve content data for ', link)
+            new_products = self.scrap(link)
+            products.extend(new_products)
+            print('Waiting to retrieve new URL')
+            time.sleep(30)
+        return products
+
+    def scrap(self, page: str) -> list[Product]:
         content = self.__cache.get_page(page)
         site = BeautifulSoup(content, 'html.parser')
+        
+        url = urlparse(page)
+        original_host = url.scheme + '://' + url.netloc
+        links = site.find_all('a')
+        for link in links:
+            href = link.get('href')
+            if href is not None:
+                if href.find('page=') > 0:
+                    link_url = original_host + href
+                    if link_url not in self.__links:
+                        self.__links.append(link_url)
 
         strategy = self.__scrapping_strategy_factory.select_strategy(page)
         products = strategy.scrap_product(site)
+        return products
 
 location = './cache'
 cache = Cache(location)
-scrapper = Scrapper(cache)
-scrapper.scrap(links[0])
+scrapper = Scrapper(cache, links)
+products = scrapper.scrap_all_sites()
--- strategy/magazineluiza.py
+++ strategy/magazineluiza.py
@@ -8,14 +8,48 @@ class MagazineLuizaScrappingStrategy(ScrappingStrategy):
     def scrap_product(self, content: BeautifulSoup) -> list[Product]:
         list_of_products: list[Product] = []
         products = content.find_all(attrs={"data-testid": "product-card-container"})
-        for product in products:
-            link = product.get('href')
-            product_content = product.find(attrs={"data-testid": "product-card-content"})
-            product_title = product_content.find(attrs={"data-testid": "product-title"}).string
-            product_review_div = product_content.find(attrs={"data-testid": "review"})
-            product_review = product_review_div.find('span').string
-            # price_original = product_content.find(attrs={"data-testid": "price-original"}).string
-            price_value = product_content.find(attrs={"data-testid": "price-value"}).get_text()
-            installment = product_content.find(attrs={"data-testid": "installment"}).get_text()
-            list_of_products.append(Product(link, product_title, product_review, price_value, installment))
+        for raw_product in products:
+            product = self.get_product(raw_product)
+            list_of_products.append(product)
         return list_of_products
+
+
+    def get_product(self, raw_product):
+        link = self.get_link(raw_product)
+        title = self.get_title(raw_product)
+        review = self.get_review(raw_product)
+        price_value = self.get_price_value(raw_product)
+        installment = self.get_installment(raw_product)
+        return Product(link, title, review, price_value, installment)
+
+    def get_link(self, raw_product):
+        return raw_product.get('href')
+
+    def get_title(self, raw_product):
+        product_content = raw_product.find(attrs={"data-testid": "product-card-content"})
+        if product_content is not None:
+            product_title = product_content.find(attrs={"data-testid": "product-title"}).string
+            return product_title
+        return None
+
+    def get_review(self, raw_product):
+        product_content = raw_product.find(attrs={"data-testid": "product-card-content"})
+        product_review_div = product_content.find(attrs={"data-testid": "review"})
+        if product_review_div is not None:
+            product_review = product_review_div.find('span').string
+            return product_review
+        return None
+    
+    def get_price_value(self, raw_product):
+        product_content = raw_product.find(attrs={"data-testid": "product-card-content"})
+        if product_content is not None:
+            price_value = product_content.find(attrs={"data-testid": "price-value"}).get_text()
+            return price_value
+        return None
+ 
+    def get_installment(self, raw_product):
+        product_content = raw_product.find(attrs={"data-testid": "product-card-content"})
+        if product_content is not None:
+            installment = product_content.find(attrs={"data-testid": "installment"}).get_text()
+            return installment
+        return None
