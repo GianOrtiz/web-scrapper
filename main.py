234b1c86eb37071b24bb21b2eec77d084662043e
Gian Ortiz <brksgian@gmail.com>
2024-11-03 21:46:15 -0300

    Dump all products in a JSON file to later be used in another projects

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import json
+
 from scrapper.scrapper import Scrapper
 from data.cache import Cache
 
 links = [
-    # "https://www.magazineluiza.com.br/lava-e-seca/eletrodomesticos/s/ed/ela1/?page=1",
-    # "https://www.zoom.com.br/lavadora-roupas/lava-e-seca",
+    "https://www.magazineluiza.com.br/lava-e-seca/eletrodomesticos/s/ed/ela1/?page=1",
+    "https://www.zoom.com.br/lavadora-roupas/lava-e-seca",
     "https://lista.mercadolivre.com.br/eletrodomesticos/lavadores/maquinas-lavar/maquina-lava-e-seca_NoIndex_True"
 ]    
 location = './cache'
@@ -12,3 +14,10 @@ if __name__ == '__main__':
     cache = Cache(location)
     scrapper = Scrapper(cache, links)
     products = scrapper.scrap_all_sites()
+
+    # Write products retrieved to a JSON file.
+    with open('products.json', 'w+') as file:
+        products_json = []
+        for product in products:
+            products_json.append(product.to_json())
+        json.dump(products_json, file, indent=2)
