191eed628ba150d396395ae0dcef21222d73ab43
Gian Ortiz <brksgian@gmail.com>
2024-11-16 08:15:21 -0300

    Add the zoom url to the links

--- scrapper/strategy/zoom.py
+++ scrapper/strategy/zoom.py
@@ -10,7 +10,7 @@ class ZoomScrappingStrategy(ScrappingStrategy):
         # Retrieve all products card in a list page.
         products = content.find_all(attrs={"data-testid": "product-card::card"})
         for raw_product in products:
-            product = self.get_product(raw_product)
+            product = self.get_product(raw_product, page)
             products_list.append(product)
 
         # Retrieves news links from this page as a crawler.
@@ -18,16 +18,23 @@ class ZoomScrappingStrategy(ScrappingStrategy):
         link_aggregator = link_aggregator_factory.select_strategy(page)
         link_aggregator.aggregate_links(content, page, links)
 
-    def get_product(self, raw_product) -> Product:
-        link = self.get_link(raw_product)
+    def get_product(self, raw_product, page) -> Product:
+        link = self.get_link(raw_product, page)
         title = self.get_title(raw_product)
         review = self.get_review(raw_product)
         price_value = self.get_price_value(raw_product)
         installment = self.get_installment(raw_product)
         return Product(link, title, review, price_value, installment)
 
-    def get_link(self, raw_product):
+    def get_link(self, raw_product, page):
         link = raw_product.get('href')
+
+        # Some links does not include the host and use relative path, we need
+        # to add the host as the prefix.
+        if 'zoom.com.br' not in link:
+            url = urlparse(page)
+            original_host = url.scheme + '://' + url.netloc
+            link = original_host + link
         return link
 
     def get_title(self, raw_product):
